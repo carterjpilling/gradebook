ce5c9fc42226db41b675651b11162e67323f3138
Cj <carterjpilling@gmail.com>
2020-12-11 15:30:30 -0700

    Initial Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var grades = new List<double>() { 12.72, 7.53, 1.11 };
+      grades.Add(55.14);
+
+      var result = 0.0;
+      foreach (var number in grades)
+      {
+        result += number;
+      }
+
+      result /= grades.Count;
+      Console.WriteLine($"The average grade is {result:N1}");
+
+
+
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello, {args[0]}!");
+      }
+      else
+      {
+        Console.WriteLine("Hello!");
+      }
+    }
+  }
+}
