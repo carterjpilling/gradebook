6bbfa5fce3e21c8fd1a8de84c7fb855e7a71dac2
Cj <carterjpilling@gmail.com>
2020-12-14 15:31:57 -0700

    Added Statistics method to Book.cs

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -11,11 +12,30 @@ namespace GradeBook
       //This is an implicit variable that can be used inside variables and constructors. You use it when you want to define the object that is currently being operated on. Had to use this because we used the name word twice. 
       this.name = name;
     }
+
+
+
     public void AddGrade(double grade)
     {
       grades.Add(grade);
     }
 
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+
+      foreach (var number in grades)
+      {
+        lowGrade = Math.Min(number, lowGrade);
+        highGrade = Math.Max(number, highGrade);
+        result += number;
+      }
+
+      result /= grades.Count;
+      Console.WriteLine($"The average grade is {result:N1}. The highest is {highGrade} and the lowest is {lowGrade}.");
+    }
     //Reason that this list is not public is because we want to user to use AddGrade so we can check their numbers before it touches our List. 
     private List<double> grades;
     private string name;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,22 +9,15 @@ namespace GradeBook
     static void Main(string[] args)
 
     {
+
+
+
       var book = new Book("Cj's Grade Book");
       book.AddGrade(59.1);
       book.AddGrade(90.5);
+      book.AddGrade(80.5);
+      book.ShowStatistics();
 
-
-      var grades = new List<double>() { 12.72, 7.53, 1.11 };
-      grades.Add(55.14);
-
-      var result = 0.0;
-      foreach (var number in grades)
-      {
-        result += number;
-      }
-
-      result /= grades.Count;
-      Console.WriteLine($"The average grade is {result:N1}");
     }
   }
 
