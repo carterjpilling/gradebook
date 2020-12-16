6c456538936c1834dda8fe44bbc183ad5417e2fc
Cj <carterjpilling@gmail.com>
2020-12-15 17:31:26 -0700

    Finished module 7

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,17 +9,44 @@ namespace GradeBook
     static void Main(string[] args)
 
     {
-
-
-
       var book = new Book("Cj's Grade Book");
-      book.AddGrade(59.1);
-      book.AddGrade(90.5);
-      book.AddGrade(80.5);
+
+
+      while (true)
+      {
+        Console.WriteLine("Enter a grade or 'q' to quite");
+        var input = Console.ReadLine();
+        if (input == "q")
+        {
+          break;
+        }
+
+        //Catching an exception
+        try
+        {
+          var grade = double.Parse(input);
+          book.AddGrade(grade);
+        }
+        catch (ArgumentException ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+        catch (FormatException ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+          //Always executes
+          Console.WriteLine("**");
+        }
+      }
+      //Loop, enter new grade. 
 
       var stats = book.GetStatistics();
 
       Console.WriteLine($"The average grade is {stats.Average:N1}. The highest is {stats.High} and the lowest is {stats.Low}.");
+      Console.WriteLine($"The letter grade is {stats.Letter}");
 
     }
   }
